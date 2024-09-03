545624b5e3ea299e18ae0f6ab86f2a3c5fc249b8
David Espino <davidespinog@gmail.com>
2024-09-02 20:55:33 -0600

    Adding authorization header on each request

--- app/lib/ApolloWrapper.js
+++ app/lib/ApolloWrapper.js
@@ -1,33 +1,32 @@
 "use client";
-// ^ this file needs the "use client" pragma
-
-import { HttpLink } from "@apollo/client";
+import { HttpLink, createHttpLink } from "@apollo/client";
 import {
   ApolloNextAppProvider,
   ApolloClient,
   InMemoryCache,
-  SSRMultipartLink,
 } from "@apollo/experimental-nextjs-app-support";
+import { setContext } from "@apollo/client/link/context";
 
-// have a function to create a client for you
 function makeClient() {
-  const httpLink = new HttpLink({
-    // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: "http://localhost:4000",
-    // you can disable result caching here if you want to
-    // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-    fetchOptions: { cache: "no-store" },
-    // you can override the default `fetchOptions` on a per query basis
-    // via the `context` property on the options passed as a second argument
-    // to an Apollo Client data fetching hook, e.g.:
-    // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
+  const httpLink = new createHttpLink({
+    uri: "http://localhost:4000"
   });
 
-  // use the `ApolloClient` from "@apollo/experimental-nextjs-app-support"
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('token');
+    console.log('token---', token)
+    return {
+      headers: {
+        ...headers, 
+        authorization: token ? `Bearer ${token}` : "",
+        test: 'my test'
+      }
+    }
+  });
+
   return new ApolloClient({
-    // use the `InMemoryCache` from "@apollo/experimental-nextjs-app-support"
     cache: new InMemoryCache(),
-    link: httpLink,
+    link: authLink.concat(httpLink),
   });
 }
 
