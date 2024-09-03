ce127c473216ed8ff236833e22c5c7167a16f2b2
David Espino <davidespinog@gmail.com>
2024-09-03 15:54:02 -0600

    Add get clients by id query

--- app/lib/ApolloWrapper.js
+++ app/lib/ApolloWrapper.js
@@ -14,7 +14,6 @@ function makeClient() {
 
   const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem('token');
-    console.log('token---', token)
     return {
       headers: {
         ...headers, 
--- app/products/page.js
+++ app/products/page.js
@@ -1,11 +1,45 @@
+"use client";
 import SectionTitle from "../components/SectionTitle";
+import { gql, useQuery } from "@apollo/client";
 
 const Products = () => {
+    const GET_CLIENTS_BY_ID = gql`
+        query getClientsBySeller {
+            getClientsBySeller {
+                id
+                name
+                lastName
+                company
+                email 
+            }
+        }`;
+    const { data, loading, error } = useQuery(GET_CLIENTS_BY_ID);
+
+    if(loading) return (<p>Loading...</p>) 
+    console.log(data);
     return (
         <>
             <SectionTitle>
                 Products
             </SectionTitle>
+            <table className="table-auto shadow-md mt-10 w-full w-lg">
+                <thead className="bg bg-gray-800">
+                    <tr className="text-white">
+                        <th className="w-1/5 py-2">Nombre</th>
+                        <th className="w-1/5 py-2">Empresa</th>
+                        <th className="w-1/5 py-2">E-mail</th>
+                    </tr>
+                </thead>
+                <tbody className="bg-white">
+                    {data?.getClientsBySeller?.map((client) => (
+                        <tr key={client.id}>
+                            <td className="border px-4 py-2">{client.name} {client.lastName}</td>
+                            <td className="border px-4 py-2">{client.company ? client.company : 'Independent'} </td>
+                            <td className="border px-4 py-2">{client.email} </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 };
