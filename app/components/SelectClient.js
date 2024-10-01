74627ab2cdbf65e910ad7e3e9532ef35e384669f
David Espino <davidespinog@gmail.com>
2024-10-01 14:46:22 -0600

    Refactor react-select component

---
+++ app/components/SelectClient.js
@@ -0,0 +1,28 @@
+import { useEffect, useState, useId } from "react";
+import Select from "react-select";
+
+const options = [
+    { value: 'b8fd3c46-0894-4ce4-b7fe-ae6872386841', label: 'Chocolate' },
+    { value: '4b4378b7-5fd3-46d6-b35a-611802616d9c', label: 'Strawberry' },
+    { value: '1dd5a0b5-6e5a-42a8-afaf-208191513462', label: 'Vanilla' }
+]
+
+const SelectClient = () => {
+    const [ isMounted, setIsMounted ] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+    const id = useId();
+    
+    return isMounted ? (
+        <>
+            <Select
+                isMulti 
+                options={options}
+                instanceId={id} 
+            />
+        </>
+    ) : null;
+};
+
+export default SelectClient;
--- app/newOrder/page.js
+++ app/newOrder/page.js
@@ -1,32 +1,16 @@
 "use client"
 import Select from "react-select";
-import { useId, useEffect, useState } from "react";
 import SectionTitle from "../components/SectionTitle";
-
-const options = [
-    { value: 'b8fd3c46-0894-4ce4-b7fe-ae6872386841', label: 'Chocolate' },
-    { value: '4b4378b7-5fd3-46d6-b35a-611802616d9c', label: 'Strawberry' },
-    { value: '1dd5a0b5-6e5a-42a8-afaf-208191513462', label: 'Vanilla' }
-]
-
+import SelectClient from "../components/SelectClient";
 
 const newOrder = () => {
-    const [ isMounted, setIsMounted ] = useState(false);
-    useEffect(() => {
-        setIsMounted(true);
-    }, [])
-    const id = useId();
     
-    return isMounted ? (
+    return (
         <>
             <SectionTitle>New order</SectionTitle>
-            <Select
-                isMulti 
-                options={options}
-                instanceId={id} 
-            />
+            <SelectClient/>
         </>
-    ) : null;
+    );
 };
 
 export default newOrder;
